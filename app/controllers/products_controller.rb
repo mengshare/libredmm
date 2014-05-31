43f9dd03a9a58597f83569c89d2db45c2e0a8876
Jun Zhou <pinepara@gmail.com>
2014-05-31 16:32:11 -0700

    find product by upcase code

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def show
-    @product = Product.find_by(code: params[:id])
+    @product = Product.find_by(code: params[:id].upcase)
     unless @product
       details = OpenDMM.search(params[:id])
       @product = Product.create(details) if details
