1f3f70536b0a1c35333e0bcc3b436e70f5ef3db8
Jun Zhou <pinepara@gmail.com>
2014-06-22 00:59:03 -0700

    fix validation error

--- app/models/product_review.rb
+++ app/models/product_review.rb
@@ -2,7 +2,7 @@ class ProductReview < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
 
-  validates :product, :user, presence: true
-  validates :product, :user, uniqueness: true
+  validates :product, uniqueness: { scope: :user }, presence: true
+  validates :user, uniqueness: { scope: :product }, presence: true
   validates :rating, inclusion: { in: 1..5 }
 end
