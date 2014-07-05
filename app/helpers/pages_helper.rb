19d30444f038a4819a8b79444a1de060d32e9f25
Jun Zhou <pinepara@gmail.com>
2014-07-04 18:50:03 -0700

    better-looking FAQ page

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,8 @@
 module PagesHelper
+  def q_and_a(question, answer)
+    [
+      content_tag(:p, class: 'question') { icon_fa 'question', question },
+      content_tag(:p, class: 'answer'  ) { icon_fa 'user', answer }
+    ].join.html_safe
+  end
 end
