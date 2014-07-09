e5374561914f48b01b7b41555dc1e1e2a54aeb73
Jun Zhou <pinepara@gmail.com>
2014-07-08 22:20:30 -0700

    fix cache

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,26 +2,18 @@ class ApiController < ApplicationController
   respond_to :json
 
   def actresses
-    respond_with cache(Product) do
-      Product.uniq.pluck(:actresses).flatten.sort.uniq
-    end
+    respond_with(cache [Product, :actresses] { Product.uniq.pluck(:actresses).flatten.sort.uniq })
   end
 
   def codes
-    respond_with cache(Product) do
-      Product.uniq.pluck(:code).sort
-    end
+    respond_with(cache [Product, :codes] { Product.uniq.pluck(:code).sort })
   end
 
   def makers
-    respond_with cache(Product) do
-      Product.uniq.pluck(:maker).sort
-    end
+    respond_with(cache [Product, :makers] { Product.uniq.pluck(:maker).sort })
   end
 
   def titles
-    respond_with cache(Product) do
-      Product.uniq.pluck(:title).sort
-    end
+    respond_with(cache [Product, :titles] { Product.uniq.pluck(:title).sort })
   end
 end
