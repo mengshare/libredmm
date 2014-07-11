5a77eab106c16cfe138e9734b4fea8e694c4f08a
Jun Zhou <pinepara@gmail.com>
2014-07-10 22:31:19 -0700

    refine cleanup migration

--- db/migrate/20140711051805_clean_mgstage_codes.rb
+++ db/migrate/20140711051805_clean_mgstage_codes.rb
@@ -3,10 +3,11 @@ class CleanMgstageCodes < ActiveRecord::Migration
     Product.all.each do |product|
       next unless product.code =~ /^\d+(\w+-\d+)$/
       if in_db = Product.search_in_db($1)
+        in_db.register_alias(product.code)
         product.destroy!
         puts "#{product.code} => #{in_db.code}\tExists!"
       elsif in_db = Product.find_by(code: $1)
-        in_db.register_alias($1)
+        in_db.register_alias(product.code, $1)
         product.destroy!
         puts "#{product.code} => #{in_db.code}\tRegistered!"
       else
