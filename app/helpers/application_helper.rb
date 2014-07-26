c033df2ff72bdf5cee5bf244fb2f26e567022f52
Jun Zhou <pinepara@gmail.com>
2014-07-26 15:32:23 -0700

    force using proxy for some sites

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,10 @@ module ApplicationHelper
   end
 
   def proxy_image(image_url)
-    use_image_proxy? ? image_url.gsub(/^http:\/\//, 'http://proxy.libredmm.com/')
-                     : image_url
+    force_proxy_hosts = [ "rsc.aurora-pro.com" ]
+    if URI(image_url).host.in? force_proxy_hosts || use_image_proxy!
+      image_url.gsub!(/^http:\/\//, 'http://proxy.libredmm.com/')
+    end
+    image_url
   end
 end
