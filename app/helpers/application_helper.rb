e6d02e6bd0f2ad9ac76bf747339c7bdbc4d78864
Jun Zhou <pinepara@gmail.com>
2014-07-27 03:53:06 -0700

    bugfix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
 
   def proxy_image(image_url)
     force_proxy_hosts = [ "rsc.aurora-pro.com" ]
-    if URI(image_url).host.in? force_proxy_hosts || use_image_proxy?
+    if URI(image_url).host.in?(force_proxy_hosts) || use_image_proxy?
       image_url.gsub!(/^http:\/\//, 'http://proxy.libredmm.com/')
     end
     image_url
