58e053e62a55afa326abf45e1acdfbe1fcabc492
Jun Zhou <pinepara@gmail.com>
2014-07-27 19:43:16 -0700

    print migration process

--- db/migrate/20140728012622_migrate_actresses.rb
+++ db/migrate/20140728012622_migrate_actresses.rb
@@ -1,11 +1,14 @@
 class MigrateActresses < ActiveRecord::Migration
   def change
     Product.find_each do |product|
+      print product.code + ':'
       product.read_attribute(:actresses).each do |name|
+        print ' ' + name
         actress = Actress.find_or_create_by(name: name)
         product.actresses << actress
       end
       product.save
+      puts ''
     end
   end
 end
